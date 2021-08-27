701ec2156a0314546078f1c5def91a159f648a5f
Chris Mo <cmochinski@gmail.com>
2021-08-27 15:28:45 -0500

    color by class temp

--- client.js
+++ client.js
@@ -9,6 +9,22 @@ $('#submit-button').on('click', appendCityToTable);
     function appendCityToTable(){
         let cityName = $('#city-name').val();//'jquery, go find our input field with the ID city-name' (can be string or number or array)
         let temperature = $('#city-temperature').val(); //'hey JQ, go find our input field with the city-temperature ID and grab it
+        let tempNumber = parseInt(temperature);
         console.log(cityName, temperature);
+
+        let rowClass = '';
+
+        if(tempNumber > 75){
+            rowClass = 'warm';
+        }
+        else if (tempNumber < 50){
+            rowClass = 'cold'; // html is just a BIG STRING...this is string-worthy
+        }
+        $('#city-table-body').append(`
+            <tr class="${rowClass}">
+                <td>${cityName}</td><td>${temperature}°</td>
+            </tr>`)
+    $('#city-name').val('');
+    $('#city-temperature').val('');
     }
 
