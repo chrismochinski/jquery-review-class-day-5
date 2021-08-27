47d6f2c2ba75152d0a6b9c3788a7a085104ed800
Chris Mo <cmochinski@gmail.com>
2021-08-27 14:46:39 -0500

    added button and connected to fields

--- client.js
+++ client.js
@@ -3,5 +3,12 @@ console.log('js');
 $(document).ready(readyNow);
 
 function readyNow() {
-    console.log('jq');
+    console.log('DOM Ready');
+$('#submit-button').on('click', appendCityToTable); 
 }
+    function appendCityToTable(){
+        let cityName = $('#city-name').val();//'jquery, go find our input field with the ID city-name' (can be string or number or array)
+        let temperature = $('#city-temperature').val(); //'hey JQ, go find our input field with the city-temperature ID and grab it
+        console.log(cityName, temperature);
+    }
+
